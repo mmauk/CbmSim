9250abd8825cd90077b791d3c2531f6e1e596a07
Sean Gallogly <sean.gallo@austin.utexas.edu>
2022-03-29 17:24:18 -0500

    extra o's

--- src/CBM_STATE_LIB/state/innetactivitystate.cpp
+++ src/CBM_STATE_LIB/state/innetactivitystate.cpp
@@ -337,7 +337,7 @@ void InNetActivityState::resetState(ActivityParams *ap)
 }
 
 void InNetActivityState::allocateMemory()
-	
+{	
 	int goTimeStep;
 	histMF = new ct_uint8_t[cp->numMF];
 	apBufMF = new ct_uint32_t[cp->numMF];
