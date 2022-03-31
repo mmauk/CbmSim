7e133628176449be1983b5a640f84bfc04984b8e
Sean Gallogly <sean.gallo@austin.utexas.edu>
2022-03-31 16:47:07 -0500

    sim object initialize in cpp pt 4

--- src/Big_Sim/control.cpp
+++ src/Big_Sim/control.cpp
@@ -44,7 +44,7 @@ void Control::runSimulationWithGRdata(int fileNum, int goRecipParam, int numTuni
 	std::fill(allGOPSTH[0], allGOPSTH[0] + numGO * (csSize + msPreCS + msPostCS), 0);
 
 	// run all trials of sim
-	runTrials(SetSim &simulation, trialTime, numTuningTrials, numGrDetectionTrials, numTrainingTrials,
+	runTrials(SetSim simulation, trialTime, numTuningTrials, numGrDetectionTrials, numTrainingTrials,
 		simNum, csSize, goMin, GOGR, GRGO, MFGO, csMinRate, csMaxRate, gogoW, spillFrac)
 	
 	// Save Data 
